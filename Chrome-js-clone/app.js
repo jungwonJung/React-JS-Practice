cf525986b314ca4d1e74128d48180429e623e760
jungwonJung <wjdwjd1501@gmail.com>
2021-09-18 12:00:00 +0900

    공부중

--- Chrome-js-clone/app.js
+++ Chrome-js-clone/app.js
@@ -8,41 +8,58 @@ const h1 = document.getElementById("title");
 
 // console.dir(title)
 
+// function handleTitleClick() {
+//   h1.style.color = "blue";
+//   if (h1.style.color === "blue") {
+//     document.body.style.backgroundColor = "blue";
+//     h1.style.color = "tomato";
+//   } else {
+//     document.body.style.backgroundColor = "tomato";
+//   }
+// }
+
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-  h1.innerText = "마우스 여기있다";
-}
+// function handleMouseEnter() {
+//   h1.innerText = "마우스 여기있다";
+// }
 
-function handleMouseLeave() {
-  h1.innerText = " 마우스 떠낫다!";
-}
+// function handleMouseLeave() {
+//   h1.innerText = " 마우스 떠낫다!";
+// }
 
-function handelWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
+// function handelWindowResize() {
+//   document.body.style.backgroundColor = "tomato";
+// }
 
-function handleWindowCopy() {
-  alert("복사 안됨");
-}
+// function handleWindowCopy() {
+//   alert("복사 안됨");
+// }
 
-function handleWindowOffline() {
-  alert("접속 끊김");
-}
+// function handleWindowOffline() {
+//   alert("접속 끊김");
+// }
 
-function handleWindowOnline() {
-  alert("접속중입니다");
-}
+// function handleWindowOnline() {
+//   alert("접속중입니다");
+// }
 
 title.addEventListener("click", handleTitleClick);
 // title.onclick = handleTitleClick;
-title.addEventListener("mouseenter", handleMouseEnter);
+// title.addEventListener("mouseenter", handleMouseEnter);
 // title.onmouseenter = handleMouseEnter;
-title.addEventListener("mouseleave", handleMouseLeave);
+// title.addEventListener("mouseleave", handleMouseLeave);
 
-window.addEventListener("resize", handelWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("offline", handleWindowOnline);
+// window.addEventListener("resize", handelWindowResize);
+// window.addEventListener("copy", handleWindowCopy);
+// window.addEventListener("offline", handleWindowOffline);
+// window.addEventListener("offline", handleWindowOnline);
