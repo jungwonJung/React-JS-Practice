5014e5209b0ff8aa1dec12d2f96c32091571ac4f
jungwonJung <wjdwjd1501@gmail.com>
2021-09-21 12:00:00 +0900

    3자리 자연수중 최소값 구하기

---
+++ JS-Algorithm/section-1/3개의숫자 최소값 구하기.js
@@ -0,0 +1,24 @@
+function solution(a, b, c) {
+  let answer;
+  if (a < b) answer = a;
+  else answer = b;
+  if (c < answer) answer = c;
+  return answer;
+}
+
+console.log(solution(2, 5, 1));
+
+function solution2(a, b, c) {
+  let answer;
+  if (a < b) {
+    answer = a;
+  } else {
+    answer = b;
+  }
+  if (c < answer) {
+    answer = c;
+  }
+  return answer;
+}
+
+console.log(solution2(6, 5, 11));
