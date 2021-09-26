7708bc807523c918da0d10398b28c5236d578620
jungwonJung <wjdwjd1501@gmail.com>
2021-09-27 00:08:30 +0900

    img js add

---
+++ Chrome-js-clone/tutorial2/js/background.js
@@ -0,0 +1,9 @@
+const images = ["이미지.jpg", "이미지2.jpg"];
+
+const chosenImages = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImages}`;
+
+document.body.appendChild(bgImage);
