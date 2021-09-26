664eade65b9c2f13eb413faec6c20c23370ab003
jungwonJung <wjdwjd1501@gmail.com>
2021-09-27 00:22:11 +0900

    집중력 하락 todo 리스트 만드는중

---
+++ Chrome-js-clone/tutorial2/js/todo.js
@@ -0,0 +1,20 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todo-list");
+
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  todoList.appendChild(li);
+}
+
+function handleTodoSubmit(e) {
+  e.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  paintTodo(newTodo);
+}
+
+todoForm.addEventListener("submit", handleTodoSubmit);
