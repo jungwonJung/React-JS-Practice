79d1c129d9c939fa9e6ad34a01e4bf8e189cbd17
jungwonJung <wjdwjd1501@gmail.com>
2021-09-29 00:23:57 +0900

    저장과 삭제

--- Chrome-js-clone/tutorial2/js/todo.js
+++ Chrome-js-clone/tutorial2/js/todo.js
@@ -2,11 +2,28 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+let todos = [];
+
+function saveTodo() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+function deleteTodo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
   todoList.appendChild(li);
 }
 
@@ -14,7 +31,21 @@ function handleTodoSubmit(e) {
   e.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
+  todos.push(newTodo);
   paintTodo(newTodo);
+  saveTodo();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+function sayHello(item) {
+  console.log("hello");
+}
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const paresdTodos = JSON.parse(savedTodos);
+  todos = paresdTodos;
+  paresdTodos.forEach((item) => console.log("투두리스트" + item));
+}
